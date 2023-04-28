31e45ebda87a19ac90b49ffcddaa1a03c01b527b
martoko <18300234+martoko@users.noreply.github.com>
2023-04-29 01:04:34 +0900

    Use `createRoot`

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import * as ReactDOM from 'react-dom'
-import { GameFrame } from './GameFrame';
-import { UI } from './UI';
-import React from 'react';
-import { PixiRoot } from './PixiRoot';
+import {GameFrame} from './GameFrame';
+import {UI} from './UI';
+import * as React from 'react';
+import {PixiRoot} from './PixiRoot';
+import {createRoot} from 'react-dom/client';
 
-const root = document.createElement('div');
-document.body.appendChild(root);
-ReactDOM.render(
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(
 	<GameFrame>
-		<UI />
-		<PixiRoot />
-	</GameFrame>,
-	root
+		<UI/>
+		<PixiRoot/>
+	</GameFrame>
 );
