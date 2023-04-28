4c39f00da0764d5de2f8cc9edb9aec5720e1be10
Denys Victorovich Matveev <strigon33@gmail.com>
2023-04-28 20:39:59 +0900

    Initial react setup

---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+
+module.exports = env => {
+	if (!env) {
+		env = {};
+	}
+	return {
+		entry: ['./src/main.sass', "./src/index.tsx"],
+		devtool: "source-map",
+        mode: "development",
+		devServer: {
+			historyApiFallback: true
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: "ts-loader"
+						}
+					]
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						"style-loader",
+						"@teamsupercell/typings-for-css-modules-loader",
+						{
+							loader: "css-loader",
+							options: {
+								modules: true,
+							}
+						},
+						"sass-loader"
+					],
+				},
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				base: "/",
+				title: "",
+			})
+		]
+	};
+};
