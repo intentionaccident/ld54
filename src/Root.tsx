2b67341a80857276e31e2dc288f1072a74c329d8
martoko <18300234+martoko@users.noreply.github.com>
2023-09-30 18:45:34 +0900

    Initial lane

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,46 @@
 import * as React from "react";
 import * as PIXI from "pixi.js";
-import { GameFrame } from "./GameFrame";
-import { UIRoot } from "./UIRoot";
-import { PixiRoot } from "./PixiRoot";
-import { createBox } from "./Box";
-import { CrateType, createCrate } from "./Crate";
+import {GameFrame} from "./GameFrame";
+import {UIRoot} from "./UIRoot";
+import {PixiRoot} from "./PixiRoot";
+import {createBox} from "./Box";
+import {CrateType, createCrate} from "./Crate";
+
+export function addLaneGraphics(app: PIXI.Application) {
+	const laneCount = 3;
+	const tileCount = 8;
+	const itemSlotWidth = 50;
+	const itemSlotHeight = 50;
+	for (let row = 0; row < laneCount; row++) {
+		const spacing = 30;
+
+		const topMargin = (
+			app.renderer.height / app.stage.scale.y
+			- laneCount * itemSlotHeight - (laneCount - 1) * spacing
+		) / 2;
+		const lane = new PIXI.Container();
+		lane.y = topMargin + row * (itemSlotHeight + spacing);
+		for (let col = 0; col < tileCount; col++) {
+			const slot = createBox(
+				itemSlotWidth, itemSlotHeight,
+				0x9CA28A
+			);
+			slot.x = col * itemSlotWidth;
+			lane.addChild(slot);
+
+			if (row == 0) {
+				const button = createBox(itemSlotWidth, 25, 0xffffff);
+				button.y = -button.height;
+				slot.addChild(button);
+			} else if (row == laneCount - 1) {
+				const button = createBox(itemSlotWidth, 25, 0xffffff);
+				button.y = itemSlotHeight;
+				slot.addChild(button);
+			}
+		}
+		app.stage.addChild(lane);
+	}
+}
 
 export function Root() {
 	const app = new PIXI.Application({
@@ -22,6 +58,7 @@ export function Root() {
 	bunny.anchor.x = 0.5;
 	bunny.anchor.y = 0.5;
 	app.stage.addChild(bunny);
+	addLaneGraphics(app);
 	app.ticker.add(() => {
 		bunny.rotation += 0.01;
 	});
@@ -35,8 +72,8 @@ export function Root() {
 
 	return <div>
 		<GameFrame>
-			<UIRoot />
-			<PixiRoot app={app} />
+			<UIRoot/>
+			<PixiRoot app={app}/>
 		</GameFrame>
 	</div>;
 }
