4d3b856ca15cc1917e52ae587bc5d93c2e2d308b
Denys Victorovich Matveev <strigon33@gmail.com>
2023-09-30 18:07:36 +0900

    Added crate template

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import * as PIXI from "pixi.js";
-import {GameFrame} from "./GameFrame";
-import {UIRoot} from "./UIRoot";
-import {PixiRoot} from "./PixiRoot";
-import {createBox} from "./Box";
+import { GameFrame } from "./GameFrame";
+import { UIRoot } from "./UIRoot";
+import { PixiRoot } from "./PixiRoot";
+import { createBox } from "./Box";
+import { CrateType, createCrate } from "./Crate";
 
 export function Root() {
 	const app = new PIXI.Application({
@@ -17,6 +18,7 @@ export function Root() {
 	const bunny = new PIXI.Sprite(texture);
 	bunny.x = app.renderer.width / app.stage.scale.x / 2;
 	bunny.y = app.renderer.height / app.stage.scale.y / 2;
+
 	bunny.anchor.x = 0.5;
 	bunny.anchor.y = 0.5;
 	app.stage.addChild(bunny);
@@ -24,10 +26,17 @@ export function Root() {
 		bunny.rotation += 0.01;
 	});
 
+	for (const type of Object.values(CrateType).filter(Number.isInteger) as CrateType[]) {
+		const item = createCrate(type)
+		item.graphics.x = app.renderer.width / app.stage.scale.x * Math.random();
+		item.graphics.y = app.renderer.height / app.stage.scale.y * Math.random();
+		app.stage.addChild(item.graphics)
+	}
+
 	return <div>
 		<GameFrame>
-			<UIRoot/>
-			<PixiRoot app={app}/>
+			<UIRoot />
+			<PixiRoot app={app} />
 		</GameFrame>
 	</div>;
 }
