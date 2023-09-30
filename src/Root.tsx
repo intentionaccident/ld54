a3c0cda85461062e17d63226974e349de02eae8d
Denys Victorovich Matveev <strigon33@gmail.com>
2023-09-30 23:42:29 +0900

    Add boat motion

--- src/Root.tsx
+++ src/Root.tsx
@@ -179,7 +179,7 @@ function tick(lanes: Lane[], action: Action) {
 	spawnCrateLine(lanes.map(l => l.slots[0]));
 }
 
-function moorBoat(boat: Boat, moorIndex: number) {
+function moorBoat(boat: Boat, moorIndex: number, lanes: Lane[], mooredBoats: Boat[], app: PIXI.Container) {
 	boat.graphics.rotation = Math.PI / 8 * 3
 	boat.graphics.x = VIEW_WIDTH - (getBoatLength(boat) * Math.cos(boat.graphics.rotation))
 		- moorIndex * (getBoatLength(boat) * Math.cos(boat.graphics.rotation) + getBoatWidth(boat) * Math.sin(boat.graphics.rotation));
@@ -188,6 +188,34 @@ function moorBoat(boat: Boat, moorIndex: number) {
 	boat.graphics.y = initialPosition
 	boat.graphics.on("mouseenter", () => boat.graphics.y -= deltaY)
 	boat.graphics.on("mouseleave", () => boat.graphics.y = initialPosition)
+
+	mooredBoats[moorIndex] = boat
+
+	boat.graphics.on("click", () => {
+		for (const lane of lanes) {
+			lane.addBoatButton.visible = !lane.boat
+			lane.addBoatButton.off("click")
+			if (!lane.addBoatButton.visible) {
+				continue
+			}
+
+			lane.addBoatButton.on("click", () => {
+				moveBoatToLane(boat, lane)
+				lane.boat = boat
+				const removedBoat = mooredBoats.findIndex(m => m === boat)
+				unmoorBoat(boat)
+
+				mooredBoats[removedBoat] = createBoat(3)
+				app.addChild(mooredBoats[removedBoat].graphics)
+
+				moorBoat(mooredBoats[removedBoat], removedBoat, lanes, mooredBoats, app)
+				for (const lane of lanes) {
+					lane.addBoatButton.visible = false
+					lane.addBoatButton.off("click")
+				}
+			})
+		}
+	})
 }
 
 export function Root() {
@@ -211,12 +239,11 @@ export function Root() {
 		bunny.rotation += 0.01;
 	});
 
-	const item = createBoat(3)
 	const mooredBoats: Boat[] = []
 	for (let i = 0; i < 2; i++) {
 		const boat = createBoat(3)
+		moorBoat(boat, i, lanes, mooredBoats, app.stage)
 		app.stage.addChild(boat.graphics)
-		moorBoat(boat, i)
 	}
 
 	return <div>
@@ -225,4 +252,17 @@ export function Root() {
 			<PixiRoot app={app} />
 		</GameFrame>
 	</div>;
+}
+
+function moveBoatToLane(boat: Boat, lane: Lane) {
+	boat.graphics.rotation = 0
+	boat.graphics.y = 0
+	boat.graphics.x = 500
+	lane.graphics.addChild(boat.graphics)
+}
+
+function unmoorBoat(boat: Boat) {
+	boat.graphics.interactive = false
+	boat.graphics.off("mouseenter")
+	boat.graphics.off("mouseleave")
 }
