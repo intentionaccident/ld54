d19b9eec0c3bf7d0a5cf25e48e154f25187ff5c3
martoko <18300234+martoko@users.noreply.github.com>
2023-09-30 19:07:24 +0900

    Buttons

--- src/Box.ts
+++ src/Box.ts
@@ -2,12 +2,17 @@ import * as PIXI from "pixi.js";
 
 export function createBox(
 	width: number, height: number,
-	color: PIXI.ColorSource
+	color: PIXI.ColorSource, interactive: boolean = false
 ): PIXI.Graphics {
 	const graphics = new PIXI.Graphics();
 	graphics.beginFill(color);
 	const lineWidth = 1;
 	graphics.lineStyle(lineWidth, 0x000000);
 	graphics.drawRect(lineWidth / 2, lineWidth / 2, width - lineWidth / 2, height - lineWidth / 2);
+	if (interactive) {
+		graphics.interactive = true;
+		graphics.eventMode = 'static';
+		graphics.hitArea = new PIXI.Rectangle(0, 0, width, height)
+	}
 	return graphics;
 }
