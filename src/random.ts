29623a8a11fff33910e0c3009d985c84253ef21a
martoko <18300234+martoko@users.noreply.github.com>
2023-10-01 15:34:57 +0900

    Add 2 sized boats

--- src/random.ts
+++ src/random.ts
@@ -12,3 +12,9 @@ export function weightedSample<T>(options: [number, T][]): T {
 
 	throw new Error("Unreachable.");
 }
+
+export function randomInt(min: number, max: number): number {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
